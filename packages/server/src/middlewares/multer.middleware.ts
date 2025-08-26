c1f2e55cdfc924398edc9a9c595507f278644ed1
Dhruv Moradiya <153066027+dhruv-moradiya@users.noreply.github.com>
2025-08-26 10:10:37 +0000

    fix:server : add cross-env as dependencies

--- packages/server/src/middlewares/multer.middleware.ts
+++ packages/server/src/middlewares/multer.middleware.ts
@@ -2,7 +2,7 @@ import multer, { FileFilterCallback } from "multer"
 
 const storage = multer.memoryStorage()
 
-const fileFilter = (req:Express.Request, file:Express.Multer.File, cb: FileFilterCallback) => {
+const fileFilter = (_req:Express.Request, file:Express.Multer.File, cb: FileFilterCallback) => {
   if (file.mimetype.startsWith("image/")) {
     cb(null, true);
   } else {
